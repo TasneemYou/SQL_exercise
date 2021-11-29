d350286aa304dd44cffa83283c80e44e9172c384
Tasneem Yousuf <tasneem.myousuf@gmail.com>
2021-11-28 19:40:53 -0500

    My solutions for exercise 1

--- chinook_exercise.sql
+++ chinook_exercise.sql
@@ -17,28 +17,44 @@ You can download it here: https://drive.google.com/file/d/0Bz9_0VdXvv9bWUtqM0NBY
 /* TASK I
 Which artists did not make any albums at all? Include their names in your answer.
 */
+select Name from artists Art cross join albums Alb on Art.ArtistId = Alb.ArtistId
+where Alb.AlbumId=NULL;
 
 
 /* TASK II
 Which artists recorded any tracks of the Latin genre?
 */
+select distinct Art.Name from artists Art join albums Alb 
+on Art.ArtistId = Alb.ArtistId join tracks t ON
+t.AlbumId = Alb.AlbumId join genres g on 
+g.GenreId = t.GenreId
+where g.Name = 'Latin';
+
 
 
 /* TASK III
 Which video track has the longest length?
 */
-
+select Name from tracks order by Milliseconds desc limit 1; 
 
 /* TASK IV
 Find the names of customers who live in the same city as the top employee (The one not managed by anyone).
 */
-
+select c.FirstName, c.LastName, c.SupportRepId, c.City, e.EmployeeId, e.ReportsTo, e.City 
+from customers c cross join employees e
+where e.ReportsTo = NULL or c.City=e.City;
 
 /* TASK V
 Find the managers of employees supporting Brazilian customers.
 */
+select m.FirstName as manager_first, m.LastName as manager_last from employees e join employees m
+on e.ReportsTo = m.EmployeeId where e.EmployeeId = (
+select SupportRepId from customers where Country = 'Brazil');
 
 
 /* TASK VI
 Which playlists have no Latin tracks?
 */
+select distinct p.Name, t.GenreId from playlists p join playlist_track pt on p.PlaylistId = pt.PlaylistId
+join tracks t on pt.TrackId = t.TrackId
+where t.GenreId != (SELECT GenreId from genres where Name = 'Latin');
